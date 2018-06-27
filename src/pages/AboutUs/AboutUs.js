ac4ac96b92a4fce5778810a66e2782e424abfbd1
dearW <1029219450@qq.com>
2018-06-27 18:28:54 +0800

    公司资质

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import Nav from '@/components/Nav';
 import TopImg from '@/components/TopImg';
 import Footer from '@/components/Footer';
+import { data } from './AboutUsData';
 
 import topImg from '@/assets/images/join_us_bg.png';
 
 class AboutUs extends Component {
+  showImg(datas) {
+    const views = [];
+    if(datas) {
+      datas.map((item, key) => {
+        views.push(
+          <div className="about-us-item">
+             <div className="about-us-item-imgdiv">
+               <img src={item.img} alt="" className={ item.type === 'column' ? 'columnImg' : 'rowImg'}/>
+              </div>
+             <div className="about-us-item-title">{item.title}</div>
+          </div>
+        )
+      })
+    }
+    return views;
+  }
   render() {
     return (
       <div className="JoinUs">
@@ -14,8 +31,8 @@ class AboutUs extends Component {
         </div>
         <div className="JoinUs-body">
           <TopImg topImg={topImg} title="公司资质" subTitle="Company qualification" />
-          <div className="JoinUs-content">
-
+          <div className="about-us">
+              {this.showImg(data)}
           </div>
           <Footer name="contact" />
         </div>
