c13fb96863b5b4104a39e22c52afb87820fc95b1
dearW <1029219450@qq.com>
2018-06-28 18:48:53 +0800

    组成

---
+++ src/components/CompositionBody/index.js
@@ -0,0 +1,18 @@
+import './style.css';
+import React, { Component } from 'react';
+
+class CompositionBody extends Component {
+  render() {
+    return (
+      <div className="CompositionBody">
+        <img src={this.props.leftImg} alt="" />
+        <div className="compositionBody-right">
+            <div className="compositionBody-right-title"><div />{this.props.title}</div>
+            <div className="compositionBody-right-content">{this.props.content}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CompositionBody;
--- src/pages/MagicWG/MagicWG.js
+++ src/pages/MagicWG/MagicWG.js
@@ -3,11 +3,22 @@ import Nav from '@/components/Nav';
 import TopImgForMagic from '@/components/TopImgForMagic';
 import Footer from '@/components/Footer';
 import ProductIntroduction from '@/components/ProductIntroduction';
+import CommonTitle from '@/components/CommonTitle';
+import CompositionBody from '@/components/CompositionBody';
+import { compositionData } from './MagicWGData';
 
 import topImg from '@/assets/images/magic-wg-bg.png';
 
 class MagicWG extends Component {
-  
+  showCompositionData(data) {
+    const views = [];
+    if(data) {
+      data.map((item, key) => {
+        views.push(<CompositionBody leftImg={item.img} title={item.title} content={item.content} key={key} />);
+      })
+    }
+    return views;
+  }
   render() {
     return (
       <div className="JoinUs">
@@ -20,6 +31,19 @@ class MagicWG extends Component {
             title="产品简介" 
             content={[' 兵棋是各方使用代表战场的棋盘和军事力量的棋子，依据从战争实战经验中总结出的规则，对作战过程进行对抗推演的工具。通过对战争的深刻理解，以对抗方式对战争动态演化过程以及不确定性和偶然性对战争的影响进行推演，具有良好的对抗性、灵活性、逻辑性和严密性，可以深化对战争的认识，启发对问题的思考，找到解决问题的途径，成为研究战争、预测战争的重要工具。', '计算机兵棋推演系统主要用于各军兵种全程序、全要素、全内容作战行动推演，在动态对抗中，提高参训者的分析判断、指挥决策、特情处置、协调控制等综合能力。为作战指挥、军事训练、分队战术等教学实践提供专业平台，为受训者提供实时动态的战场态势、近似实战的训练环境和客观科学的评价手段，使训练由静态变为动态，实现教学资源整合和教学过程控制，增强教与学的互动，优化教学训练模式，提高教学训练效果']}
           />
+
+          <div className="magic-wg-system-composition">
+            <CommonTitle title="系统组成" />
+            {this.showCompositionData(compositionData)}
+          </div>
+
+          <div>
+            <CommonTitle title="产品图片" />
+          </div>
+
+          <div>
+            <CommonTitle title="技术特点" />
+          </div>
           
           <Footer name="contact" />
         </div>
