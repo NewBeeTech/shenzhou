2924ee366298b01ad7c73d904bcdacc4b490fa73
dearW <1029219450@qq.com>
2018-06-30 22:14:31 +0800

    军工案例

--- src/pages/MilitaryIndustrialEnterprises/MilitaryIndustrialEnterprises.js
+++ src/pages/MilitaryIndustrialEnterprises/MilitaryIndustrialEnterprises.js
@@ -4,12 +4,10 @@ import TopImgForMagic from '@/components/TopImgForMagic';
 import Footer from '@/components/Footer';
 import ProductIntroduction from '@/components/ProductIntroduction';
 import CommonTitle from '@/components/CommonTitle';
-import CharacteristicsRow from '@/components/CharacteristicsRow'
 import Carousel from '@/components/Carousel'
-import { projectData } from './MilitaryIndustrialEnterprisesData';
+import { projectData, compositionData } from './MilitaryIndustrialEnterprisesData';
 
-import topImg from '@/assets/images/magic-st-bg.png';
-import magicImg from '@/assets/images/magic-st-img1.png';
+import topImg from '@/assets/images/military-industrial-enterprises-bg.png';
 
 class MilitaryIndustrialEnterprises extends Component {
   render() {
@@ -19,32 +17,19 @@ class MilitaryIndustrialEnterprises extends Component {
           <Nav selected="contact" />
         </div>
         <div className="JoinUs-body">
-          <TopImgForMagic topImg={topImg} title="MAXSim仿真平台" subTitle="MAXSIM SIMULATION PLATFORM" />
+          <TopImgForMagic topImg={topImg} title="军工企业案例" subTitle="MILITARY ENTERPRISE CASE" />
           <ProductIntroduction 
             title="产品简介" 
             content={
-              ['MAXSim是我公司基于先进的GBB技术和DMAS技术的系统架构研发完成的自主可控分布式仿真平台，实现了巨大实体数量（100万个）的实时仿真，它具有精细全面的装备建模能力、可视化行为建模能力和强大的运行管理能力。MAXSim仿真平台提供了装备建模、行为建模、想定制作、运行监控、过程回放等一系列工具，支持模型建立、想定制作、仿真运行、数据分析等重要过程。 ']}
+              ['公司致力于为军工企业提供软件平台产品和应用开发服务。基于分布式仿真平台MAXSim、虚拟仿真平台MagicBook、交互式电子手册MagiclETM、虚拟场景系统MagicScene等平台，为多家军工企业产品前期论证、产品设计、产品操作训练、产品维修保障的全流程提供多方面服务，完成了多个成功案例。']}
           />
-          <div className="magic-st-system-composition-img">
-              <img src={magicImg} alt="" />
-          </div>
-
-          <div className="magic-st-system-composition">
-            <ProductIntroduction 
-              title="系统功能" 
-              content={
-                ['MAXSim平台覆盖了实况仿真、虚拟仿真和构造仿真领域，可广泛的应用于各军兵种战役战术仿真和武器装备论证。MAXSim所提供的开发接口（SDK），具有良好的开放性和扩展性，能很好地支持各种定制开发。']}
-            />
-          </div>
-
+          
           <div className="magic-wg-product-img">
             <CommonTitle title="产品图片" color="#fff"/>
-            {/* <Carousel data={projectData} /> */}
+            <Carousel data={projectData} />
           </div>
 
-          <div className="magic-st-technical">
-            <CommonTitle title="典型案例" />
-          </div>
+          <div className="magic-st-technical" />
           
           <Footer name="contact" />
         </div>
--- src/pages/SpaceSituation/SpaceSituation.js
+++ src/pages/SpaceSituation/SpaceSituation.js
@@ -4,14 +4,27 @@ import TopImgForMagic from '@/components/TopImgForMagic';
 import Footer from '@/components/Footer';
 import ProductIntroduction from '@/components/ProductIntroduction';
 import CommonTitle from '@/components/CommonTitle';
-import CharacteristicsRow from '@/components/CharacteristicsRow'
+import Characteristics from '@/components/Characteristics'
 import Carousel from '@/components/Carousel'
-import { projectData } from './SpaceSituationData';
+import { projectData, compositionData } from './SpaceSituationData';
 
-import topImg from '@/assets/images/magic-st-bg.png';
-import magicImg from '@/assets/images/magic-st-img1.png';
+import topImg from '@/assets/images/space-situation-bg.png';
 
 class SpaceSituation extends Component {
+  showCompositionData(data) {
+    const views = [];
+    if(data) {
+      data.map((item, key) => {
+        views.push(
+          <div key={key} className="space-situation-system-system-item">
+            <div><img src={item.img} alt=""/></div>
+            <div className="space-situation-system-system-title">{item.title}</div>
+          </div>
+        );
+      })
+    }
+    return views;
+  }
   render() {
     return (
       <div className="JoinUs">
@@ -19,31 +32,31 @@ class SpaceSituation extends Component {
           <Nav selected="contact" />
         </div>
         <div className="JoinUs-body">
-          <TopImgForMagic topImg={topImg} title="MAXSim仿真平台" subTitle="MAXSIM SIMULATION PLATFORM" />
+          <TopImgForMagic topImg={topImg} title="空间态势感知信息保障仿真系统" subTitle="SPATIAL SITUATIONAL AWARENESS INFORMATION SUPPORT SIMULATION SYSTEM" />
           <ProductIntroduction 
             title="产品简介" 
             content={
-              ['MAXSim是我公司基于先进的GBB技术和DMAS技术的系统架构研发完成的自主可控分布式仿真平台，实现了巨大实体数量（100万个）的实时仿真，它具有精细全面的装备建模能力、可视化行为建模能力和强大的运行管理能力。MAXSim仿真平台提供了装备建模、行为建模、想定制作、运行监控、过程回放等一系列工具，支持模型建立、想定制作、仿真运行、数据分析等重要过程。 ']}
+              ['本系统是一个集空间目标资源状态监测、空间目标资源需求分析、冲突排查、应急支持以及空间目标资源网常规管理于一体并相互关联、协调的计算机辅助决策支持系统。系统可将评估资源分配中的知识和经验，资源分配的基本原则，系统优化的基本思想等综合起来，构成系统知识库，再将进行评估资源分配时的思路和人工智能技术结合起来，构成系统推理机。根据评估空间目标资源状态信息，分析获得的数据信息，通过推理机调用有关的规则，最终给出评估结论。']}
           />
-          <div className="magic-st-system-composition-img">
-              <img src={magicImg} alt="" />
-          </div>
-
-          <div className="magic-st-system-composition">
-            <ProductIntroduction 
-              title="系统功能" 
-              content={
-                ['MAXSim平台覆盖了实况仿真、虚拟仿真和构造仿真领域，可广泛的应用于各军兵种战役战术仿真和武器装备论证。MAXSim所提供的开发接口（SDK），具有良好的开放性和扩展性，能很好地支持各种定制开发。']}
-            />
+          <div className="magic-st-system-composition combat-platform-system-composition">
+            <CommonTitle title="系统功能"/>
+            <div className="space-situation-system-system">
+              {this.showCompositionData(compositionData)}
+            </div>
           </div>
 
           <div className="magic-wg-product-img">
             <CommonTitle title="产品图片" color="#fff"/>
-            {/* <Carousel data={projectData} /> */}
+            <Carousel data={projectData} />
           </div>
 
-          <div className="magic-st-technical">
-            <CommonTitle title="典型案例" />
+          <div className="magic-wg-technical">
+            <CommonTitle title="技术特点" />
+            <div className="helicopter-system-technical-content">
+              <div><Characteristics num="01" content="仿真模型精度高。采用已有的成熟算法，对每个算法单元进行单元测试，保证每个算法的计算误差和解算时间在要求的范围内。"/></div>
+              <div><Characteristics num="02" content="空间目标数量大。结合仿真平台和卫星仿真平台，一方面通过仿真平台充分利用CPU的指令执行能力和内存的高吞吐量，另一方面通过卫星仿真平台利用GPU的强大并行计算能力和图形计算能力，组成CPU+GPU的强力组合，全面利用可用的硬件资源，极大提升了系统可承载的实体数量。" /></div>
+              <div><Characteristics num="03" content="系統业务性强。本系统涉及到空间仿真和评估的各个方面，任务覆盖面大、业务性强。 "/></div>
+            </div>
           </div>
           
           <Footer name="contact" />
